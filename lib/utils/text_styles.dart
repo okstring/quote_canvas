9da443769538758b668c903dc6946b659889fe4f
okstring <milkoi7801@gmail.com>
2025-03-18 23:30:55 +0900

    feat: AppTextStyles 구현

---
+++ lib/utils/text_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle titleLarge = TextStyle(
+    fontFamily: 'Pretendard',
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle bodyText = TextStyle(
+    fontFamily: 'Pretendard',
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle quote = TextStyle(
+    fontFamily: 'Pretendard',
+    fontSize: 32,
+    fontWeight: FontWeight.w900,
+  );
+}
