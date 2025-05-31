d0a0490e5ecf0fef4a765eaa8c96d5ec819f2224
okstring <milkoi7801@gmail.com>
2025-05-31 18:09:54 +0900

    feat: 실제 광고 id 추가

--- lib/core/helper/ad_helper.dart
+++ lib/core/helper/ad_helper.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 
 class AdHelper {
   static String get interstitialAdUnitId {
-    //TODO: 실제 광고 ID로 바꿔야 함
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/1033173712';
+      return 'ca-app-pub-6198172205965396/5836858996';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/4411468910';
     } else {
